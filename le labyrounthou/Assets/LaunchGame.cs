063e24275967858d29fd54f969b533f40f73b259
TauntiO <titouan.br64@gmail.com>
2023-01-21 16:53:42 +0100

    Menu

---
+++ le labyrounthou/Assets/LaunchGame.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class LaunchGame : MonoBehaviour
+{
+    public void LaunchingGame()
+    {
+        //Le code pour lancer la scène du jeu
+        Debug.Log("Launching game");
+    }
+}
---
+++ le labyrounthou/Assets/LaunchSettings.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class LaunchSettings : MonoBehaviour
+{
+    public void LaunchingSettings()
+    {
+        //Le code pour lancer la scène des settings
+        Debug.Log("Lauching settings");
+    }
+}
